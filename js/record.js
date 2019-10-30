c8eff80e17ba7c18f43385dcdb1e4c41fa534de1
Charlie <laujf@hotmail.com>
2019-10-30 17:12:44 +0800

    v1.2
    Recording JS done. CSS left

--- js/record.js
+++ js/record.js
@@ -4,6 +4,7 @@ URL = window.URL || window.webkitURL;
 var gumStream; 						//stream from getUserMedia()
 var rec; 							//Recorder.js object
 var input; 							//MediaStreamAudioSourceNode we'll be recording
+var x = document.getElementById("pageBeginCountdown").value;
 
 // shim for AudioContext when it's not avb. 
 var AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -16,12 +17,35 @@ var pauseButton = document.getElementById("pauseButton");
 
 //add events to those 2 buttons
 //recordButton.addEventListener("click", startRecording); //calling startRecording() function from index.html onclick event , so this line of code is not needed anymore
+
 stopButton.addEventListener("click", stopRecording);
 pauseButton.addEventListener("click", pauseRecording);
 
+
+// THIS PART OF CODE HAVE NOT DONE YET !!!!!! PLEASE MODIFY ASAP !!!!!!!!!!!!!!!!
 aud.onended = function(){
 	startRecording();
-};
+
+	document.querySelector("#myText").innerHTML = "Recording...";
+	document.querySelector("#pageBeginCountdownText").innerHTML = "";
+	
+	setTimeout(function(){
+		stopRecording();
+		document.querySelector("#myText").innerHTML = "Finished recording.";
+	}, x*1000);
+
+}
+
+navigator.mediaDevices.getUserMedia({audio:true})
+	.then(function(stream){
+		audioContext = new AudioContext();
+		gumStream = stream;
+		input = audioContext.createMediaStreamSource(stream);
+		rec = new Recorder(input,{numChannels:1})
+	}).catch(function(err){
+		console.log('getusermedia failed');
+	})
+
 
 function startRecording() {
 	console.log("recordButton clicked");
@@ -45,45 +69,12 @@ function startRecording() {
     	We're using the standard promise based getUserMedia() 
     	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
 	*/
-	
-
-	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
-
-		/*
-			create an audio context after getUserMedia is called
-			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
-			the sampleRate defaults to the one set in your OS for your playback device
-
-		*/
-		audioContext = new AudioContext();
-
-		//update the format 
-		document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
-
-		/*  assign to gumStream for later use  */
-		gumStream = stream;
-		
-		/* use the stream */
-		input = audioContext.createMediaStreamSource(stream);
-
-		/* 
-			Create the Recorder object and configure to record mono sound (1 channel)
-			Recording 2 channels  will double the file size
-		*/
-		rec = new Recorder(input,{numChannels:1})
 
 		//start the recording process
 		rec.record()
 
 		console.log("Recording started");
 
-	}).catch(function(err) {
-	  	//enable the record button if getUserMedia() fails
-    	recordButton.disabled = false;
-    	stopButton.disabled = true;
-    	pauseButton.disabled = true
-	});
 }
 
 function pauseRecording(){
